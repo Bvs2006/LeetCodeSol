c0f4b2e2a3b6439f7bccd31267218c2052aa5a8b
Venkata Srujith Bellamkonda <bvsrujith2006@gmail.com>
2026-08-06 12:13:24 +0530

    Time: 0 ms (100%), Space: 8.5 MB (73.4%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-int bvs(int n,int t){
+int bvs(int n){
     int pro=1;
-    for(;n>0;n=n/10)
+    while(n>0)
     {
         pro*=n%10;
+        n=n/10;
     }
     return pro;
 }
     int smallestNumber(int n, int t) {
-        while(bvs(n,t)%t!=0)
+        while(bvs(n)%t!=0)
         {
             n++;
         }
